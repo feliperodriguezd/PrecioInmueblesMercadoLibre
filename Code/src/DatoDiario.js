04c5690051191ae131e78ae33c1fa60499211758
feliperodriguezd <72653242+feliperodriguezd@users.noreply.github.com>
2024-04-28 00:02:45 -0300

    creacion del nuevo sistema de calculo diario de precios

---
+++ Code/src/DatoDiario.js
@@ -0,0 +1,74 @@
+import { ObtenerDia, ObtenerMes, ObtenerAnio, EnviarMensaje, VerificarSiElMensajeSeEnvio } from "./FuncionesAuxiliares";
+import { telegramToken, telegramChatId } from "./tokens";
+
+
+async function AgregarADByEnviarMensaje(client) {
+    let datos = DatosDelaDB(client)
+    let precioCasa = PrecioPromedioCasa(datos)
+    let precioApartamento = PrecioPromedioApartamento(datos)
+    await InsertarEnDB(client, precioCasa, precioApartamento)
+
+    const telegramUrl = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
+
+    let PreciosPromediosDivididos = [];
+    let PrecioSinMiles = Math.round(precioCasa % 1000);
+    let PrecioEnMiles = Math.round((precioCasa - PrecioSinMiles) / 1000);
+    PreciosPromediosDivididos[0] = PrecioEnMiles;
+    PreciosPromediosDivididos[1] = PrecioSinMiles;
+    PrecioSinMiles = Math.round(precioApartamento % 1000);
+    PrecioEnMiles = Math.round((precioApartamento - PrecioSinMiles) / 1000);
+    PreciosPromediosDivididos[2] = PrecioEnMiles;
+    PreciosPromediosDivididos[3] = PrecioSinMiles;
+
+    let mensajeTelegram = MensajeTelegram(PreciosPromediosDivididos);
+
+    const respuestaTelegram = await EnviarMensaje(telegramUrl, telegramChatId, mensajeTelegram);
+
+    await VerificarSiElMensajeSeEnvio(respuestaTelegram);
+}
+
+function PrecioPromedioCasa(datos) {
+    let cantidadDeDatos = datos.length
+    let sumaPromedio = 0
+    for (let i = 0; i < cantidadDeDatos; i++) {
+        sumaPromedio += datos[i].preciocasa
+    }
+    return sumaPromedio / cantidadDeDatos
+}
+
+function PrecioPromedioApartamento(datos) {
+    let cantidadDeDatos = datos.length
+    let sumaPromedio = 0
+    for (let i = 0; i < cantidadDeDatos; i++) {
+        sumaPromedio += datos[i].precioapartamento
+    }
+    return sumaPromedio / cantidadDeDatos
+}
+
+async function DatosDelaDB(client) {
+    return await client.query(`select distinct * FROM datosdiariomultiple`);
+}
+
+async function InsertarEnDB(client, PrecioCasa, PrecioApartamento) {
+    await client.query(`INSERT INTO DatosSemanales (Fecha, preciocasa, precioapartamento) VALUES ('${ObtenerDia()}/${ObtenerMes()}/${ObtenerAnio()}' , ${PrecioCasa}, ${PrecioApartamento})`);
+}
+
+function MensajeTelegram(PreciosPromediosDivididos) {
+    let telegramMessage = `Precios promedio en MercadoLibre:\nCasa: U$S ${PreciosPromediosDivididos[0]}.`;
+
+    if (PreciosPromediosDivididos[1] < 100) {
+        telegramMessage = telegramMessage + `0${PreciosPromediosDivididos[1]}\nApartamento: U$S ${PreciosPromediosDivididos[2]}.`;
+    } else {
+        telegramMessage = telegramMessage + `${PreciosPromediosDivididos[1]}\nApartamento: U$S ${PreciosPromediosDivididos[2]}.`;
+    }
+
+    if (PreciosPromediosDivididos[3] < 100) {
+        telegramMessage = telegramMessage + `0${PreciosPromediosDivididos[3]}`;
+    } else {
+        telegramMessage = telegramMessage + `${PreciosPromediosDivididos[3]}`;
+    }
+    return telegramMessage;
+}
+
+
+export { AgregarADByEnviarMensaje }
--- Code/src/PrecioPorMinuto.js
+++ Code/src/PrecioPorMinuto.js
@@ -1,51 +1,21 @@
-import { ObtenerDia, ObtenerMes, ObtenerAnio, ObtenerDatosDeURL, PasarDatosAJSON, SeLlegoAlLimiteDeOffset, EnviarMensaje, VerificarSiElMensajeSeEnvio, DatosDePropiedades, CantidadDePropiedadesEnLista } from "./FuncionesAuxiliares";
+import { ObtenerDatosDeURL, PasarDatosAJSON, SeLlegoAlLimiteDeOffset, DatosDePropiedades, CantidadDePropiedadesEnLista } from "./FuncionesAuxiliares";
 import { telegramToken, telegramChatId } from "./tokens";
 
 async function CalculoDePrecioPropiedades(client) {
-  const telegramUrl = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
-
   let codigosCategoria = ["MLU1468", "MLU1474"];
-  let PreciosPromediosDivididos = [];
   let Precios = []
   for (let i = 0; i < codigosCategoria.length; i++) {
     let PrecioSuma = await ObtenerSumaDePrecioPropiedades(codigosCategoria[i]);
     let cantidadDeCasasVistas = 1050;
     let PrecioPromedio = PrecioSuma / cantidadDeCasasVistas;
-    let PrecioSinMiles = Math.round(PrecioPromedio % 1000);
-    let PrecioEnMiles = Math.round((PrecioPromedio - PrecioSinMiles) / 1000);
-    PreciosPromediosDivididos[i * 2] = PrecioEnMiles;
-    PreciosPromediosDivididos[(i * 2) + 1] = PrecioSinMiles;
     Precios[i] = Math.round(PrecioPromedio);
   }
 
   await InsertarEnDB(client, Precios[0], Precios[1]);
-
-  let mensajeTelegram = MensajeTelegram(PreciosPromediosDivididos);
-
-  const respuestaTelegram = await EnviarMensaje(telegramUrl, telegramChatId, mensajeTelegram);
-
-  await VerificarSiElMensajeSeEnvio(respuestaTelegram);
 }
 
 async function InsertarEnDB(client, PrecioCasa, PrecioApartamento) {
-  await client.query(`INSERT INTO DatosSemanales (Fecha, preciocasa, precioapartamento) VALUES ('${ObtenerDia()}/${ObtenerMes()}/${ObtenerAnio()}' , ${PrecioCasa}, ${PrecioApartamento})`);
-}
-
-function MensajeTelegram(PreciosPromediosDivididos) {
-  let telegramMessage = `Precios promedio en MercadoLibre:\nCasa: U$S ${PreciosPromediosDivididos[0]}.`;
-
-  if (PreciosPromediosDivididos[1] < 100) {
-    telegramMessage = telegramMessage + `0${PreciosPromediosDivididos[1]}\nApartamento: U$S ${PreciosPromediosDivididos[2]}.`;
-  } else {
-    telegramMessage = telegramMessage + `${PreciosPromediosDivididos[1]}\nApartamento: U$S ${PreciosPromediosDivididos[2]}.`;
-  }
-
-  if (PreciosPromediosDivididos[3] < 100) {
-    telegramMessage = telegramMessage + `0${PreciosPromediosDivididos[3]}`;
-  } else {
-    telegramMessage = telegramMessage + `${PreciosPromediosDivididos[3]}`;
-  }
-  return telegramMessage;
+  await client.query(`INSERT INTO datosdiariomultiple (preciocasa, precioapartamento) VALUES ('${PrecioCasa}, ${PrecioApartamento})`);
 }
 
 async function ObtenerSumaDePrecioPropiedades(codigoCategoria) {
