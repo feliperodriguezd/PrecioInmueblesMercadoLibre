e1edd543ecb73378f8ffbc9169b20b956b38dbc4
feliperodriguezd <72653242+feliperodriguezd@users.noreply.github.com>
2024-05-21 15:24:46 -0300

    new functionality
    los datos de la base de datoss se descargan solos

--- Data/UpdateData.py
+++ Data/UpdateData.py
@@ -1,15 +1,59 @@
 import psycopg2
+import openpyxl
+import os
+
 import ConnectionString
 
-conn = psycopg2.connect(ConnectionString.connection_string)
+def OpenExcel():
+    try:
+        excel = openpyxl.load_workbook(GetRelativePath('Datos.xlsx'))
+    except:
+        print("Error al abrir el excel")
+    return excel
 
-cur = conn.cursor()
-query = "SELECT * FROM datossemanales order by SPLIT_PART(fecha, '/', 3), CAST(SPLIT_PART(fecha, '/', 2) AS INTEGER), CAST(SPLIT_PART(fecha, '/', 1) AS INTEGER)"
-cur.execute(query)
-row = cur.fetchone()
-while row != None:
-    print(row)
+def Edit(sheet, data):
+    for numbre in range(0, len(data)):
+        row = numbre + 2
+        sheet[f'A{str(row)}'] = data[numbre][0]
+        sheet[f'B{str(row)}'] = data[numbre][1]
+        sheet[f'C{str(row)}'] = data[numbre][2]
+    row = len(data) + 1
+    last = len(data) - 1
+    sheet[f'A{str(row)}'] = data[last][0]
+    sheet[f'B{str(row)}'] = data[last][1]
+    sheet[f'C{str(row)}'] = data[last][2]
+
+def OpenAndEdit(data):
+    excel = OpenExcel()
+    sheet = excel.active     
+    try:
+        Edit(sheet, data)
+        excel.save(GetRelativePath('Datos.xlsx')) 
+    except Exception as ex:
+        print(f"Error: {ex}")
+
+def GetData():
+    data = []
+    connection = psycopg2.connect(ConnectionString.connection_string)
+    cur = connection.cursor()
+    query = "SELECT * FROM datossemanales order by SPLIT_PART(fecha, '/', 3), CAST(SPLIT_PART(fecha, '/', 2) AS INTEGER), CAST(SPLIT_PART(fecha, '/', 1) AS INTEGER)"
+    cur.execute(query)
     row = cur.fetchone()
-cur.close()
-conn.close()
+    while row != None:
+        rowData = []
+        rowData.append(row[0])
+        rowData.append(row[1])
+        rowData.append(int(row[2]))
+        data.append(rowData)
+        row = cur.fetchone()
+    cur.close()
+    connection.close()
+    return data
 
+def GetRelativePath(relativePath):
+    absolutePath = os.path.dirname(__file__)
+    fullPath = os.path.join(absolutePath, relativePath)
+    return fullPath
+
+data = GetData()
+OpenAndEdit(data)
